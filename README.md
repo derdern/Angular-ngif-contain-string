e4c096638250f640f74e4bbdd26e1033c9b28e99
derdern <tderdern@gmail.com>
2020-03-11 16:23:19 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hjfprz-h1sves
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hjfprz-h1sves)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens special sale'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'Best phone special sale'
+  },
+  {
+    name: 'Phone Samsung',
+    price: 399,
+    description: 'more best phone'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
